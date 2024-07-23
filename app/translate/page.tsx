2e6ebf673876d4df1b41b7ee1260caaaf460f24e
Hugo Hsi <hhsi@hugodehsign.com>
2024-07-22 22:08:01 -0400

    added ai to the project

--- app/translate/page.tsx
+++ app/translate/page.tsx
@@ -1,3 +1,11 @@
-export default function page() {
-  return <div>page</div>;
+import { model } from '@/ai/gemini';
+
+export default async function page() {
+  const prompt = 'Write a story about an AI and magic';
+
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
+  return <div>{text}</div>;
 }
